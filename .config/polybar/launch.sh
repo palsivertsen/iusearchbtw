c1c323cdff17f17e71c8e205f722516803b41f6b
Pal Sivertsen <palsivertsen@gmail.com>
2022-04-24 13:22:35 +0200

    polybar: Fix launch script, can now be launched from cmd line

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,13 +3,13 @@
 LOG_DIR=/var/log/polybar/
 
 # Terminate already running bar instances
-killall -q polybar
+killall -wq polybar
 
 # Launch
+export MONITOR
 for MONITOR in $(xrandr --listactivemonitors | tail -n+2 | grep -Po '[-\w]*$'); do
-    export MONITOR
-    echo "---" >> $LOG_DIR/main-$MONITOR.log
-    polybar -r main 2>> $LOG_DIR/main-$MONITOR.log &
+    echo "---" >> "$LOG_DIR/main-$MONITOR.log"
+    polybar -r main >> "$LOG_DIR/main-$MONITOR.log" 2>&1 &
 done
 
 echo "Bars launched..."
