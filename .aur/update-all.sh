8a831c1da647a90175b435ab5d0fb8da73bed1b7
Pal Sivertsen <palsivertsen@gmail.com>
2022-12-08 22:42:37 +0100

    aur: Sort updates

--- .aur/update-all.sh
+++ .aur/update-all.sh
@@ -11,7 +11,7 @@ function print_header {
 	echo "$border"
 }
 
-for dir in $(find -mindepth 1 -maxdepth 1 -type d); do
+for dir in $(find -mindepth 1 -maxdepth 1 -type d | sort); do
 	print_header "Updating $dir"
 	(
 		cd $dir
