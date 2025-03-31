28de31243cdfcd76fe93916a8a7c441d66bc2f70
Pal Sivertsen <palsivertsen@gmail.com>
2025-03-31 19:46:26 +0200

    Change aliases for watchexec

--- .bash_aliases.d/watch_go.sh
+++ .bash_aliases.d/watch_go.sh
@@ -1,4 +1,4 @@
 alias watchexecgotest='watchexec -rc --stop-timeout=1s -- "go test -json ./... | docker run --rm -i ghcr.io/gotesttools/gotestfmt:latest"'
 alias gotestwatchexec=watchexecgotest
-alias watchexecgolangcilint='watchexec -rc --stop-timeout=1s -- "golangci-lint run --sort-results --color always | head -n$(($(tput lines) - 3))"'
-alias golangcilitwatchexec=watchexecgolangcilint
+alias watchexecgolangci-lint-run='watchexec -rc --stop-timeout=1s -- "golangci-lint run --sort-results --color always | head -n$(($(tput lines) - 3))"'
+alias golangci-lint-run-watchexec=watchexecgolangci-lint-run
